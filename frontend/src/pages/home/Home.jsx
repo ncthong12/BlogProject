af80fd4b907e997891e5bcf35503291914cc7cd1
ncthong12 <57798899+ncthong12@users.noreply.github.com>
2022-11-19 19:33:10 +0700

    19-11

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,40 +1,38 @@
-import React, { useEffect, useState } from "react"
-import { Card } from "../../components/blog/Card"
-import { Category } from "../../components/category/Category"
-import axios from "axios"
-import { useLocation } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { Card } from "../../components/blog/Card";
+import { Category } from "../../components/category/Category";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
 
 export const Home = () => {
-  const [posts, setPosts] = useState([])
-  const [cats, setCat] = useState([])
+  const [posts, setPosts] = useState([]);
+  const [cats, setCat] = useState([]);
 
   // setp 2
-  const { search } = useLocation()
+  const { search } = useLocation();
   // const location = useLocation()
 
   useEffect(() => {
+    console.log("post call");
     const fetchPost = async () => {
-      const res = await axios.get("/posts" + search)
-      setPosts(res.data)
-    }
+      const res = await axios.get("/posts" + search);
+      setPosts(res.data);
+    };
+    fetchPost();
+  }, [search]);
+  useEffect(() => {
+    console.log("category call");
     const getCat = async () => {
-      const res = await axios.get("/category" + search)
-      setCat(res.data)
-    }
-    getCat()
-    fetchPost()
-  }, [search])
-  // useEffect(() => {
-  //   const getCat = async () => {
-  //     const res = await axios.get("/category" + search)
-  //     setCat(res.data)
-  //   }
-  //   getCat()
-  // }, [search])
+      const res = await axios.get("/category" + search);
+      setCat(res.data);
+    };
+    getCat();
+  }, []);
   return (
     <>
-      <Category cats={cats}/>
+      {console.log("render")}
+      <Category cats={cats} />
       <Card posts={posts} />
     </>
-  )
-}
+  );
+};
